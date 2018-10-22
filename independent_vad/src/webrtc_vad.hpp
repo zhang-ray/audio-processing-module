3b9760c7a2174291126a5361636cf35de7c8eebc
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-22 19:28:12 +0800

    [win32] fix dll issue

--- independent_vad/src/webrtc_vad.hpp
+++ independent_vad/src/webrtc_vad.hpp
@@ -18,15 +18,18 @@
 
 #include <stddef.h>
 #include <cstdint>
+#include "common_types.hpp"
 
 typedef struct WebRtcVadInst VadInst;
 
 // Creates an instance to the VAD structure.
+WEBRTC_DLLEXPORT
 VadInst* WebRtcVad_Create();
 
 // Frees the dynamic memory of a specified VAD instance.
 //
 // - handle [i] : Pointer to VAD instance that should be freed.
+WEBRTC_DLLEXPORT
 void WebRtcVad_Free(VadInst* handle);
 
 // Initializes a VAD instance.
@@ -35,6 +38,7 @@ void WebRtcVad_Free(VadInst* handle);
 //
 // returns        : 0 - (OK),
 //                 -1 - (NULL pointer or Default mode could not be set).
+WEBRTC_DLLEXPORT
 int WebRtcVad_Init(VadInst* handle);
 
 // Sets the VAD operating mode. A more aggressive (higher mode) VAD is more
@@ -48,6 +52,7 @@ int WebRtcVad_Init(VadInst* handle);
 // returns        : 0 - (OK),
 //                 -1 - (NULL pointer, mode could not be set or the VAD instance
 //                       has not been initialized).
+WEBRTC_DLLEXPORT
 int WebRtcVad_set_mode(VadInst* handle, int mode);
 
 // Calculates a VAD decision for the |audio_frame|. For valid sampling rates
@@ -62,6 +67,7 @@ int WebRtcVad_set_mode(VadInst* handle, int mode);
 // returns              : 1 - (Active Voice),
 //                        0 - (Non-active Voice),
 //                       -1 - (Error)
+WEBRTC_DLLEXPORT 
 int WebRtcVad_Process(VadInst* handle, int fs, const int16_t* audio_frame,
                       size_t frame_length);
 
@@ -72,6 +78,7 @@ int WebRtcVad_Process(VadInst* handle, int fs, const int16_t* audio_frame,
 // - frame_length [i] : Speech frame buffer length in number of samples.
 //
 // returns            : 0 - (valid combination), -1 - (invalid combination)
+WEBRTC_DLLEXPORT
 int WebRtcVad_ValidRateAndFrameLength(int rate, size_t frame_length);
 
 
--- independent_vad/tests/test_00/test_00.cpp
+++ independent_vad/tests/test_00/test_00.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "webrtc_vad.hpp"
 #include <fstream>
 #include <vector>
